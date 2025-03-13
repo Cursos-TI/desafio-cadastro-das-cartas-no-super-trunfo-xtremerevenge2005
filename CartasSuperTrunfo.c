4be9aeb085633c553e250dc95ff769b61a868922
Túlio Vasconcelos Sacramento <pythonqwertyadress@gmail.com>
2025-03-13 10:08:29 -0300

    Teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,8 @@
 //Teste larissa
 
 int main() {
+
+    printf("Olá, Mundo!");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
